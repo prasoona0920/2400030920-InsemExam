b48528a3cf8c720c98680b379c2b0dd98d21304b
prasoona0920 <2400030920@kluniversity.in>
2025-10-16 10:39:16 +0530

    Added missing files

---
+++ APP.js
@@ -0,0 +1,16 @@
+
+console.log("Student Portal Application Started");
+
+
+function registerStudent(name, grade) {
+    console.log(`Student ${name} from grade ${grade} has been registered.`);
+}
+
+
+function welcomeMessage() {
+    console.log("Welcome to the Online Student Portal!");
+}
+
+
+welcomeMessage();
+registerStudent("prasoona",12);
